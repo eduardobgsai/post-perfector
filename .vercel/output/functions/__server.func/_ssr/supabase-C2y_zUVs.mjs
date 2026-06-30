26bf40423754d8f5b8585073bc62dc63de19fd2a
Eduardo Rodrigues <eduardo@bgstecnologiaemarketing.com.br>
2026-06-30 15:53:03 -0300

    Fix: remover build local do vercel do git

--- .vercel/output/functions/__server.func/_ssr/supabase-C2y_zUVs.mjs
+++
@@ -1,5 +0,0 @@
-import { t as createClient } from "../_libs/supabase__supabase-js.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/supabase-C2y_zUVs.js
-var supabase = createClient("https://pxrkrwttactqkdxcysib.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4cmtyd3R0YWN0cWtkeGN5c2liIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3ODg3MTYsImV4cCI6MjA5NzM2NDcxNn0.rowTBrSIny6FeY9S6hOmooDGqSlsXgoCo2dy3vuJDnY");
-//#endregion
-export { supabase as t };
