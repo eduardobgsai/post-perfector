6a5a74270c556e62d6b62dad9e521d0112bc3e3f
Eduardo Rodrigues <eduardo@bgstecnologiaemarketing.com.br>
2026-07-01 14:43:27 -0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [
-    tsConfigPaths(),
-    tailwindcss(),
-    tanstackStart({
-      server: {
-        preset: 'vercel',
-      },
-    }),
-  ],
-})
+  tanstackStart: {
+    server: { entry: "server" },
+  },
+  nitro: {
+    preset: "vercel"
+  },
+  vite: {
+  },
+});
